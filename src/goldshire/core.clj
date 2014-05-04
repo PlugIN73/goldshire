3322566fd6513283a36034d59cae0c9a77919619
plugin73 <laplugin73@gmail.com>
2014-05-04 20:25:59 +0000

    fix bug with ruby eval

--- src/goldshire/core.clj
+++ src/goldshire/core.clj
@@ -9,12 +9,11 @@
   (container/create client {:Hostname "127.0.0.1",
                             :Memory "10m",
                             :Image "paintedfox/ruby",
-                            :Cmd ["ruby -e", "1 + 1"] }))
+                            :Cmd ["ruby", "-e", "puts 1 + 1"] }))
 
 
 (defn ruby-eval
   "eval ruby expression on docker"
   [cmd]
   (container/start client (:Id box))
-  ;;(println (container/inspect client (:Id box)))
   (slurp (container/attach client (:Id box) :logs true :stdout true :stderr true :stream true)))
