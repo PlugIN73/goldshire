44f9a7c211fd4289145f95a3b4cd1c5f2b875e2d
plugin73 <laplugin73@gmail.com>
2014-05-07 18:57:32 +0000

    enqueue and read msg to redis

--- src/goldshire/core.clj
+++ src/goldshire/core.clj
@@ -1,5 +1,6 @@
 (ns goldshire.core
   (:require [goldshire.docker :as docker-client])
+  (:require [goldshire.redis :as redis-helper])
   (:import [org.apache.commons.daemon Daemon DaemonContext])
   (:gen-class
     :implements [org.apache.commons.daemon.Daemon]))
@@ -10,8 +11,10 @@
   (swap! state assoc :running true))
 
 (defn start []
+  (redis-helper/queue-worker)
   (while (:running @state)
     (println "tick")
+    (redis-helper/enqueue "code" "puts 1 + 1")
     (Thread/sleep 2000)))
 
 (defn stop []
--- src/goldshire/redis.clj
+++ src/goldshire/redis.clj
@@ -1,10 +1,18 @@
 (ns goldshire.redis
-  (:require [taoensso.carmine :as car :refer (wcar)]))
+  (:require [taoensso.carmine :as car :refer (wcar)])
+  (:require [taoensso.carmine.message-queue :as car-mq]))
 
-(def server1-conn {:pool {<opts>} :spec {<opts>}})
-(defmacro wcar* [& body] `(car/wcar server1-conn ~@body))
+(def server-conn {:pool {} :spec {:host "127.0.0.1" :port 6379}})
+(defmacro wcar* [& body] `(car/wcar server-conn ~@body))
 
-(wcar* (car/ping)
-       (car/set "foo" "bar")
-       (car/get "foo"))
+(defn queue-worker
+  []
+  (car-mq/worker server-conn "code"
+   {:handler (fn [{:keys [message attempt]}]
+               (println "Received" message)
+               {:status :success})}))
+
+(defn enqueue
+  [queue, msg]
+  (wcar* (car-mq/enqueue queue msg)))
 
